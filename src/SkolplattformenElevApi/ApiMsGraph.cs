3da7610b5597b80f48bf91d6928f1d82370d6be0
Kajetan Kazimierczak <kajetan@hotmail.com>
2022-07-20 00:46:05 +0200

    Added Microsoft.Graph nuget. Can get info about student from graph.

---
+++ src/SkolplattformenElevApi/ApiMsGraph.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Microsoft.Graph;
+using SkolplattformenElevApi.Models.MsGraph;
+using SkolplattformenElevApi.Models.Timetable;
+
+namespace SkolplattformenElevApi;
+
+public partial class Api
+{
+
+    public async Task<string> GetMsGraphAccessTokenAsync()
+    {
+        var tokenResponse = await GetOauthTokenResponseAsync(OauthTokenResource.MsGraph);
+
+        return tokenResponse.AccessToken;
+    }
+
+
+    private async Task<OauthTokenResponse> GetOauthTokenResponseAsync(string tokenResource)
+    {
+        var temp_url = "https://elevstockholm.sharepoint.com/sites/skolplattformen/_api/SP.OAuth.Token/Acquire";
+        var content = "{\"resource\":\"" + tokenResource + "\"}";
+        var request = new HttpRequestMessage
+        {
+            RequestUri = new Uri(temp_url),
+            Method = HttpMethod.Post,
+            Headers =
+            {
+                { "Odata-version", "3.0"},
+                { "Referer", "https://elevstockholm.sharepoint.com/sites/skolplattformen/" },
+                { "X-Scope", "8a22163c-8662-4535-9050-bc5e1923df48" },
+                { "Accept", "application/json"},
+                { "Origin", "https://elevstockholm.sharepoint.com" },
+                { "X-RequestDigest", _formDigestValue}
+            },
+            Content = new StringContent(content)
+        };
+        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+
+        var temp_res = await _httpClient.SendAsync(request);
+
+        var temp_content = await temp_res.Content.ReadAsStringAsync();
+
+        var jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var obj = JsonSerializer.Deserialize<OauthTokenResponse>(temp_content, jsonSerializerOptions);
+
+        return obj;
+
+    }
+
+    public async Task<string> GetMeAsync()
+    {
+
+        var accessToken = await GetMsGraphAccessTokenAsync();
+
+
+        var graphServiceClient = new GraphServiceClient(new DelegateAuthenticationProvider((requestMessage) => {
+            requestMessage
+                .Headers
+                .Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            return Task.CompletedTask;
+        }));
+
+        var me = await graphServiceClient.Me.Request().GetAsync();
+
+        return me.DisplayName;
+    }
+}
---
+++ src/SkolplattformenElevApi/Models/MsGraph/OauthTokenResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SkolplattformenElevApi.Models.MsGraph;
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+internal class OauthTokenResponse
+{
+    [JsonPropertyName("@odata.context")]
+    public string OdataContext { get; set; }
+
+    [JsonPropertyName("@odata.type")]
+    public string OdataType { get; set; }
+
+    [JsonPropertyName("@odata.id")]
+    public string OdataId { get; set; }
+
+    [JsonPropertyName("@odata.editLink")]
+    public string OdataEditLink { get; set; }
+
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonPropertyName("expires_on")]
+    public string ExpiresOn { get; set; }
+
+    [JsonPropertyName("id_token")]
+    public object IdToken { get; set; }
+
+    [JsonPropertyName("resource")]
+    public string Resource { get; set; }
+
+    [JsonPropertyName("scope")]
+    public string Scope { get; set; }
+
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; }
+}
+
+internal static class OauthTokenResource
+{
+    public static string Bing => "https://www.bing.com";
+    public static string OutlookSeatch => "https://outlook.office365.com/search";
+    public static string Loki => "https://loki.delve.office.com";
+    public static string MsGraph => "https://graph.microsoft.com";
+
+}
