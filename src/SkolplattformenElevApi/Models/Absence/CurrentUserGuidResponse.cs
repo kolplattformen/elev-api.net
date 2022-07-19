b4cb0ef7ff9b7be3f33d39db5ec0091f5f2951e8
Kajetan Kazimierczak <kajetan-kazimierczak@users.noreply.github.com>
2022-07-19 13:05:22 +0200

    PlannedAbsence (#7)

---
+++ src/SkolplattformenElevApi/Models/Absence/CurrentUserGuidResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace SkolplattformenElevApi.Models.Absence;
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+public class CurrentUserGuidData
+{
+    public string CurrentUserGuid { get; set; }
+}
+
+public class CurrentUserGuidResponse
+{
+    public object Error { get; set; }
+    public CurrentUserGuidData Data { get; set; }
+    public object Exception { get; set; }
+    public List<object> Validation { get; set; }
+    public DateTime SessionExpires { get; set; }
+    public bool NeedSessionRefresh { get; set; }
+}
